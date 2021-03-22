00d73e73d4b179cc28bbe64f6f79ed6cf72eccc0
poopistasty <77624008+poopistasty@users.noreply.github.com>
2021-03-22 19:37:58 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,17 @@
+class Slingshot{
+constructor(bodyA,bodyB){
+    var options = {
+        bodyA:bodyA,
+        bodyB:bodyB,
+        length:10,
+        stiffness:0.5
+    }
+    this.slingshot= Constraint.create(options);
+    World.add(world, this.slingshot);
+}
+display(){
+    var pointA=this.slingshot.bodyA.position;
+    var pointB=this.slingshot.bodyB.position;
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+}
+}
